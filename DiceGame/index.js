74d26591dcf9be7ff9d6122fe358d63c6dd10f88
AmanAmazing <83318313+AmanAmazing@users.noreply.github.com>
2023-01-30 19:31:42 +0000

    Game logic is done, Need to finish styling whenever

--- DiceGame/index.js
+++ DiceGame/index.js
@@ -1,32 +1,30 @@
-//generating random Dice number between 1 and 6  
-function randomDice(){
-return Math.floor(Math.random()*6 +1)
-}
-
-
-
 //getting random dice numbers 
-const dice1 = randomDice();
-const dice2 = randomDice();
+const dice1 = Math.floor(Math.random()*6 +1);
+const dice2 = Math.floor(Math.random()*6 +1);
 
-// comparing dice numbers 
-function diceCheck(number1,number2){
-    if (number1 === number2){
-        return true;
-    }
-    else{
-        return false;
+// getting  the dice images for the players 
+const player1img = "./images/dice"+dice1+".png"
+const player2img = "./images/dice"+dice2+".png"
+
+// setting the dice image for the players 
+const diceImageSelector1 = document.querySelector(".img1");
+const diceImageSelector2 = document.querySelector(".img2");
+diceImageSelector1.setAttribute("src",player1img);
+diceImageSelector2.setAttribute("src",player2img);
+
+// checking who won 
+function winnerchecker(number1,number2){
+    mainHeading = document.querySelector(".main-heading")
+    if (number1>number2){
+        mainHeading.innerHTML = `<i class="fa-solid fa-flag-pennant"></i> Player 1 won`;         
+    }else if(number2>number1){
+        mainHeading.innerHTML =  `<i class="fa-solid fa-flag-pennant"></i> Player 2 won`; 
+    }else{
+        mainHeading.innerText =  "It was a draw"; 
     }
 }
 
-// storing result
-const result = diceCheck(dice1,dice2);
+winnerchecker(dice1,dice2);
 
 
 
-// fetching the image selector 
-diceImage1 = document.querySelector(".img1");
-diceImage2 = document.querySelector(".img2");
-
-diceImage1.setAttribute("src",`./images/dice${dice1}.png`)
-diceImage2.setAttribute("src",`./images/dice${dice2}.png`)
